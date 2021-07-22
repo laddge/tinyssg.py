2424987b26164e016b05079c891e70135df8f454
Laddge <dev.laddge@gmail.com>
2021-07-22 17:21:07 +0900

    [feat] add {src, extra} dir option

--- tinyssg.py
+++ tinyssg.py
@@ -4,14 +4,15 @@ import argparse
 
 def cmd():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--src_dir', default='src/')
     parser.add_argument('-t', '--template_dir', default='template/')
+    parser.add_argument('-e', '--extra_dir', default='extra/')
     parser.add_argument('-o', '--output_dir', default='output/')
     return parser.parse_args()
 
 
 def main():
-    tmpl, out = cmd().template_dir, cmd().output_dir
-    print(tmpl, out)
+    print(cmd())
 
 
 if __name__ == '__main__':
