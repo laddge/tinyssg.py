9e19748a78e395d43095c5dda0834523edcbb71c
Laddge <dev.laddge@gmail.com>
2021-07-22 17:32:05 +0900

    [feat] add description for cmd args help

--- tinyssg.py
+++ tinyssg.py
@@ -4,10 +4,14 @@ import argparse
 
 def cmd():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--src_dir', default='src/')
-    parser.add_argument('-t', '--template_dir', default='template/')
-    parser.add_argument('-e', '--extra_dir', default='extra/')
-    parser.add_argument('-o', '--output_dir', default='output/')
+    parser.add_argument('-s', '--src_dir', default='src/',
+                        help='Markdown Source Directory (default: src/)')
+    parser.add_argument('-t', '--template_dir', default='template/',
+                        help='Template Directory (default: template/)')
+    parser.add_argument('-e', '--extra_dir', default='extra/',
+                        help='Extra Files Directory (default: extra/)')
+    parser.add_argument('-o', '--output_dir', default='output/',
+                        help='Output Directory (default: output/)')
     return parser.parse_args()
 
 
